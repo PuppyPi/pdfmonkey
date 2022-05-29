136a7ff84fe67f5f6f142e99948e8923478e8245
PuppyPi <puppyofcodez@gmail.com>
2022-05-29 20:12:16 +0000

    Starting working on the Java side

---
+++ src/rebound/tieins/pdfplumberpuppetteer/PDFPlumberPuppeteerEmissary.java
@@ -0,0 +1,68 @@
+package rebound.tieins.pdfplumberpuppetteer;
+
+import static rebound.util.collections.CollectionUtilities.*;
+import java.io.Closeable;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import javax.annotation.Nullable;
+import rebound.osint.OSUtilities;
+import rebound.util.functional.throwing.FunctionalInterfacesThrowingCheckedExceptionsStandard.RunnableThrowingIOException;
+
+public class PDFPlumberPuppeteerEmissary
+implements Closeable
+{
+	protected final Reader fromPuppeteer;
+	protected final Writer toPuppeteer;
+	protected final RunnableThrowingIOException cleanlyWaitFor;
+	protected final RunnableThrowingIOException forciblyCleanup;
+	
+	public PDFPlumberPuppeteerEmissary(InputStream fromPuppeteer, OutputStream toPuppeteer, RunnableThrowingIOException cleanlyWaitFor, RunnableThrowingIOException forciblyCleanup)
+	{
+		this.fromPuppeteer = new InputStreamReader(fromPuppeteer);
+		this.toPuppeteer = new OutputStreamWriter(toPuppeteer);
+		this.cleanlyWaitFor = cleanlyWaitFor;
+		this.forciblyCleanup = forciblyCleanup;
+	}
+
+
+
+	@Override
+	public void close() throws IOException
+	{
+		try
+		{
+			fromPuppeteer.close();
+		}
+		finally
+		{
+			try
+			{
+				toPuppeteer.close();
+			}
+			finally
+			{
+				forciblyCleanup.close();
+			}
+		}
+	}
+	
+	
+	
+	public static PDFPlumberPuppeteerEmissary openLocal(String python3Interpreter, File pdfplumberpuppeteerFile, @Nullable String pythonpath) throws IOException
+	{
+		ProcessBuilder b = new ProcessBuilder(listof(python3Interpreter, pdfplumberpuppeteerFile.getAbsolutePath()));
+		
+		if (pythonpath != null)
+			b.environment().put("PYTHONPATH", pythonpath);
+		
+		Process p = b.start();
+		
+		InputStream fromPuppeteer = p.getInputStream();
+		OutputStream toPuppeteer = p.getOutputStream();
+		RunnableThrowingIOException waiter = OSUtilities.processWaiter(p);
+		
+		return new PDFPlumberPuppeteerEmissary(fromPuppeteer, toPuppeteer, waiter, p::destroyForcibly);
+	}
+}
