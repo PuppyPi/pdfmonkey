289b28bb6ebdc5969e09609fa5eb4e15ea377ed8
PuppyPi <puppyofcodez@gmail.com>
2022-05-30 01:06:29 +0000

    OOps, forgot to redirect the IO!  XD''

--- src/rebound/tieins/pdfplumberpuppetteer/PDFPlumberPuppeteer.java
+++ src/rebound/tieins/pdfplumberpuppetteer/PDFPlumberPuppeteer.java
@@ -14,6 +14,7 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.io.Writer;
+import java.lang.ProcessBuilder.Redirect;
 import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.Map;
@@ -68,6 +69,10 @@ implements Closeable
 		
 		if (pythonpath != null)
 			b.environment().put("PYTHONPATH", pythonpath);
+		
+		b.redirectInput(Redirect.PIPE);
+		b.redirectOutput(Redirect.PIPE);
+		b.redirectError(Redirect.INHERIT);
 		
 		Process p = b.start();
 		return new PDFPlumberPuppeteer(p);
